e5a79933271bbf38f72a2bd43c13b8f6c5fc1d57
Kalimar <kalimar.maia@gmail.com>
2014-07-28 14:41:53 -0400

    add handlebar template

--- js/main.js
+++ js/main.js
@@ -13,21 +13,28 @@ var app = {
     });
   },
 
-  initialize: function() {
-    var self = this;
-    this.store = new LocalStorageStore(function() {
-      self.showAlert('Store initialized', 'Info');
-    });
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-  },
-
   showAlert: function (message, title) {
     if (navigator.notification) {
       navigator.notification.alert(message, null, title, 'OK');
     } else {
       alert(title ? (title + ": " + message) : message);
     }
+  },
+
+  renderHomeView: function() {
+    $('body').html(html);
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+  },
+
+  initialize: function() {
+    var self = this;
+    this.homeTpl = Handlbars.compile($("#home-tpl").html());
+    this.employeeLiTpl = Handlebars.compile
+    this.store = new LocalStorageStore(function() {
+      self.renderHomeView();
+    });
   }
 };
+
 
 app.initialize();
